a227f3fbd7675e34629f16e0ff7006e71cad37b7
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-29 09:34:48 -0400

    addPost function and getPost funciton added

--- Smart_Contract/Web3Social.sol
+++ Smart_Contract/Web3Social.sol
@@ -21,4 +21,23 @@ contract web3Social {
     event postCreated (address sender, uint256 id, string postTxt, string postImg);
 
     mapping(uint256 => post) Posts;
+
+    function addPost (string memory _postTxt, string memory _postImg) public payable {
+        require(msg.value == (1 ether), "Please submit 1 ether");
+        post storage newPost = Posts[counter];
+        newPost.sender = msg.sender;
+        newPost.id = counter;
+        newPost.postTxt = _postTxt;
+        newPost.postImg = _postImg;
+        emit postCreated (msg.sender, counter, _postTxt, _postImg);
+        counter++;
+
+        payable(owner).transfer(msg.value);
+    }
+
+    function getPost(uint256 _postId) public view returns (string memory, string memory, address) {
+        require(id < counter, "Post does not exist");
+        post storage getPost = Posts[_postId];
+        return (getPost.postTxt, getPost.postImg, getPost.sender);
+    }
 }
