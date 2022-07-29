262f54fe6fd9a713f9d88ce18699ed751a9dd403
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-29 09:23:43 -0400

    smart contract contructor, variables, structs, events, mapping added

--- Smart_Contract/Web3Social.sol
+++ Smart_Contract/Web3Social.sol
@@ -5,4 +5,20 @@ pragma solidity ^0.8.7;
 contract web3Social {
     address public owner;
     uint256 private counter;
+
+    constructor() {
+        counter = 0;
+        owner = msg.sender
+    }
+
+    struct post {
+        address sender;
+        uint256 id;
+        string postTxt;
+        string postImg;
+    }
+
+    event postCreated (address sender, uint256 id, string postTxt, string postImg);
+
+    mapping(uint256 => post) Posts;
 }
