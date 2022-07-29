3f38d02efc7cdd9a77c670d20b83d5dc3a510bf7
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-29 10:09:31 -0400

    edits to depoy script

---
+++ Smart_Contract/deploy/01-deploy-web3-social.js
@@ -0,0 +1,31 @@
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+    const arguments = []
+    const web3Social = await deploy("Web3Social", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+    log("Deployed !")
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(web3Social.address, arguments)
+        log("Verified !")
+    }
+    log("----------------------------------------------------")
+}
+
+module.exports.tags = ["all", "web3social"]
