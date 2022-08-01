889b1c7dd6cd19080745a53c0de53e14d9bc1f1e
daenamcclintock <daena.mcclintock@gmail.com>
2022-08-01 19:31:12 -0400

    inital attempt to use web3 news story api, unsuccessful

--- web3_social/src/components/Rightbar.js
+++ web3_social/src/components/Rightbar.js
@@ -19,41 +19,86 @@ const Rightbar = () => {
       'X-RapidAPI-Host': 'web3-news-api.p.rapidapi.com'
     }
   };
+  
+  let newsStoriesArr = []
+  const getNewsStories = () => {
+    axios.request(options)
+      .then(function (response) {
+        console.log('this is the response data', response.data);
+        for (let i = 0; i < response.data.length; i++) {
+          if (response.data[i].title.split(" ").length > 3 && !response.data[i].title.includes("img")) {
+            newsStoriesArr.push(response.data[i])
+          }
+          else {
+            console.log("No News Stories")
+          }
+        }
+      })
+      .then(() => {
+        console.log('These are the news stories:', newsStoriesArr)
+      })
+      .catch(function (error) {
+        console.error('this is the error', error);
+      })
+  }
 
-  axios.request(options).then(function (response) {
-    console.log('this is the response data', response.data);
-  })
-  .catch(function (error) {
-    console.error('this is the error', error);
-  });
+  getNewsStories()
+
+  // const trends = [
+  //   {
+  //     img: spaceshooter,
+  //     text: "Learn how to build a Web3 FPS game using unity...",
+  //     link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-space-fps-game/",
+  //   },
+  //   {
+  //     img: netflix,
+  //     text: "The first Moralis Project! Let's Netflix and chill...",
+  //     link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-netflix-clone/",
+  //   },
+  //   {
+  //     img: academy,
+  //     text: "Master DeFi in 2022. Start  at the Moralis Academy...",
+  //     link: "https://academy.moralis.io/courses/defi-101",
+  //   },
+  //   {
+  //     img: js,
+  //     text: "Become a Web3 Developer with just simple JS...",
+  //     link: "https://academy.moralis.io/all-courses",
+  //   },
+  //   {
+  //     img: youtube,
+  //     text: "Best youtube channel to learn about Web3...",
+  //     link: "https://www.youtube.com/channel/UCgWS9Q3P5AxCWyQLT2kQhBw",
+  //   },
+  // ];
 
   const trends = [
     {
-      img: spaceshooter,
-      text: "Learn how to build a Web3 FPS game using unity...",
-      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-space-fps-game/",
+      img: "",
+      text: newsStoriesArr[0].title.toString(),
+      link: newsStoriesArr[0].url.toString(),
     },
     {
-      img: netflix,
-      text: "The fisrt Moralis Project! Let's Netflix and chill...",
-      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-netflix-clone/",
+      img: "",
+      text: newsStoriesArr[1].title.toString(),
+      link: newsStoriesArr[1].url.toString(),
     },
     {
-      img: academy,
-      text: "Master DeFi in 2022. Start  at the Moralis Academy...",
-      link: "https://academy.moralis.io/courses/defi-101",
+      img: "",
+      text: newsStoriesArr[2].title.toString(),
+      link: newsStoriesArr[2].url.toString(),
     },
     {
-      img: js,
-      text: "Become a Web3 Developer with just simple JS...",
-      link: "https://academy.moralis.io/all-courses",
+      img: "",
+      text: newsStoriesArr[3].title.toString(),
+      link: newsStoriesArr[3].url.toString(),
     },
     {
-      img: youtube,
-      text: "Best youtube channel to learn about Web3...",
-      link: "https://www.youtube.com/channel/UCgWS9Q3P5AxCWyQLT2kQhBw",
+      img: "",
+      text: newsStoriesArr[4].title.toString(),
+      link: newsStoriesArr[4].url.toString(),
     },
-  ];
+  ]
 
   return (
     <>
